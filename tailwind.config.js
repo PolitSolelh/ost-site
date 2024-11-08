ef808bcfcc4ece425cd14f63132d8007ca9fb0eb
PolitSolelh <132207294+PolitSolelh@users.noreply.github.com>
2024-11-08 10:09:55 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./index.html",],
   theme: {
     fontFamily: {
       'agencyfb': ['AgencyFB'],
+    },
+    colors: {
+      'main': '#111',
+      'tblue': '#62c8f1',
+      'tpink': '#f4a7b7',
     }
   },
   plugins: [],
